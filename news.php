c96b3bd0622ae84f563bafc8a80d35d46de3d2fc
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-02-07 15:40:55 +0100

    Suppression de la connexion à la base de données dans news.php

--- news.php
+++ news.php
@@ -70,7 +70,6 @@
                  */
 
                 // Etape 1: Ouvrir une connexion avec la base de donnée.
-                $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
                 //verification
                 // if ($mysqli->connect_errno)
                 // {
