f86b01eb870f0f8cecf0a0f9ccccc208f90333e5
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-02-08 16:25:17 +0100

    INCLUDE mis partout

--- news.php
+++ news.php
@@ -40,26 +40,7 @@
                 </section>
             </aside>
             <main>
-                <!-- L'article qui suit est un exemple pour la présentation et 
-                  @todo: doit etre retiré -->
-                <article>
-                    <h3>
-                        <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
-                    </h3>
-                    <address>par AreTirer</address>
-                    <div>
-                        <p>Ceci est un paragraphe</p>
-                        <p>Ceci est un autre paragraphe</p>
-                        <p>... de toutes manières il faut supprimer cet 
-                            article et le remplacer par des informations en 
-                            provenance de la base de donnée (voir ci-dessous)</p>
-                    </div>                                            
-                    <footer>
-                        <small>♥1012 </small>
-                        <a href="">#lorem</a>,
-                        <a href="">#piscitur</a>,
-                    </footer>
-                </article>               
+                         
 
                 <?php
                 
