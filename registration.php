dc08128bd1cf9226790174779f024765987b8f06
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-02-13 10:43:28 +0100

    Supprimer la section de présentation dans registration.php

--- registration.php
+++ registration.php
@@ -33,11 +33,6 @@
     </nav>
 
         <div id="wrapper" >
-
-            <aside>
-                <h2>Présentation</h2>
-                <p>Bienvenue sur notre réseau social.</p>
-            </aside>
             <main>
                 <article>
                     <h2>Inscription</h2>
@@ -53,7 +48,7 @@
                         // on ne fait ce qui suit que si un formulaire a été soumis.
                         // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travaille se situe
                         // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
-                        echo "<pre>" . print_r($_POST, 1) . "</pre>";
+                        // echo "<pre>" . print_r($_POST, 1) . "</pre>";
                         // et complétez le code ci dessous en remplaçant les ???
                         $new_email = $_POST['email'];
                         $new_alias = $_POST['pseudo'];
--- usurpedpost.php
+++ usurpedpost.php
@@ -1,121 +1,121 @@
-<?php
-session_start();
-?>
+<?php session_start(); ?>
+<?php include "connection.php"; ?>
+
 <!doctype html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>ReSoC - Post d'usurpateur</title> 
-        <meta name="author" content="Julien Falconnet">
-        <link rel="stylesheet" href="style.css"/>
-    </head>
-    <body>
-        <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">Profil</a>
+
+<head>
+    <meta charset="utf-8">
+    <title>Send - Usurpation ??</title>
+    <link rel="shortcut icon" href="sendico.ico" type="image/x-icon">
+    <meta name="author" content="Chac">
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <div class="headerPS">
+        <a href="admin.php"><img src="resoc.png" alt="Logo de Pride Score" width="30" height=auto></a>
+        <h1>Send</h1>
+    </div>
+    <nav aria-label="Navigation">
+        <ul>
+            <li><a href="news.php">Actualités</a></li>
+            <li><a href="wall.php?user_id=5">Mur</a></li>
+            <li><a href="feed.php?user_id=5">Flux</a></li>
+            <li><a href="tags.php?tag_id=1">Mots-clés</a></li>
+            <li><a href="#">▾ Profil</a>
                 <ul>
                     <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
+                    <li><a href="followers.php?user_id=5">Mes followers</a></li>
                     <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
                 </ul>
+            </li>
+        </ul>
+    </nav>
 
-            </nav>
-        </header>
+    <div id="wrapper">
 
-        <div id="wrapper" >
+        <aside>
+            <h2>Présentation</h2>
+            <p>Sur cette page on peut poster un message en se faisant
+                passer pour quelqu'un d'autre</p>
+        </aside>
+        <main>
+            <article>
+                <h2>Poster un message</h2>
+                <?php
+                /**
+                 * BD
+                 */
+                $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+                /**
+                 * Récupération de la liste des auteurs
+                 */
+                $listAuteurs = [];
+                $laQuestionEnSql = "SELECT * FROM users";
+                $lesInformations = $mysqli->query($laQuestionEnSql);
+                while ($user = $lesInformations->fetch_assoc()) {
+                    $listAuteurs[$user['id']] = $user['alias'];
+                }
 
-            <aside>
-                <h2>Présentation</h2>
-                <p>Sur cette page on peut poster un message en se faisant 
-                    passer pour quelqu'un d'autre</p>
-            </aside>
-            <main>
-                <article>
-                    <h2>Poster un message</h2>
-                    <?php
-                    /**
-                     * BD
-                     */
-                    $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
-                    /**
-                     * Récupération de la liste des auteurs
-                     */
-                    $listAuteurs = [];
-                    $laQuestionEnSql = "SELECT * FROM users";
-                    $lesInformations = $mysqli->query($laQuestionEnSql);
-                    while ($user = $lesInformations->fetch_assoc())
-                    {
-                        $listAuteurs[$user['id']] = $user['alias'];
+
+                /**
+                 * TRAITEMENT DU FORMULAIRE
+                 */
+                // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
+                // si on recoit un champs email rempli il y a une chance que ce soit un traitement
+                $enCoursDeTraitement = isset($_POST['auteur']);
+                if ($enCoursDeTraitement) {
+                    // on ne fait ce qui suit que si un formulaire a été soumis.
+                    // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travaille se situe
+                    // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
+                    echo "<pre>" . print_r($_POST, 1) . "</pre>";
+                    // et complétez le code ci dessous en remplaçant les ???
+                    $authorId = $_POST['auteur'];
+                    $postContent = $_POST['message'];
+
+
+                    //Etape 3 : Petite sécurité
+                    // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
+                    $authorId = intval($mysqli->real_escape_string($authorId));
+                    $postContent = $mysqli->real_escape_string($postContent);
+                    //Etape 4 : construction de la requete
+                    $lInstructionSql = "INSERT INTO posts "
+                        . "(id, user_id, content, created, permalink, post_id) "
+                        . "VALUES (NULL, "
+                        . $authorId . ", "
+                        . "'" . $postContent . "', "
+                        . "NOW(), "
+                        . "'', "
+                        . "NULL);";
+                    echo $lInstructionSql;
+                    // Etape 5 : execution
+                    $ok = $mysqli->query($lInstructionSql);
+                    if (!$ok) {
+                        echo "Impossible d'ajouter le message: " . $mysqli->error;
+                    } else {
+                        echo "Message posté en tant que :" . $listAuteurs[$authorId];
                     }
+                }
+                ?>
+                <form action="usurpedpost.php" method="post">
+                    <input type='hidden' name='???' value='achanger'>
+                    <dl>
+                        <dt><label for='auteur'>Auteur</label></dt>
+                        <dd><select name='auteur'>
+                                <?php
+                                foreach ($listAuteurs as $id => $alias)
+                                    echo "<option value='$id'>$alias</option>";
+                                ?>
+                            </select></dd>
+                        <dt><label for='message'>Message</label></dt>
+                        <dd><textarea name='message'></textarea></dd>
+                    </dl>
+                    <input type='submit'>
+                </form>
+            </article>
+        </main>
+    </div>
+</body>
 
-
-                    /**
-                     * TRAITEMENT DU FORMULAIRE
-                     */
-                    // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
-                    // si on recoit un champs email rempli il y a une chance que ce soit un traitement
-                    $enCoursDeTraitement = isset($_POST['auteur']);
-                    if ($enCoursDeTraitement)
-                    {
-                        // on ne fait ce qui suit que si un formulaire a été soumis.
-                        // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travaille se situe
-                        // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
-                        echo "<pre>" . print_r($_POST, 1) . "</pre>";
-                        // et complétez le code ci dessous en remplaçant les ???
-                        $authorId = $_POST['???'];
-                        $postContent = $_POST['???'];
-
-
-                        //Etape 3 : Petite sécurité
-                        // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
-                        $authorId = intval($mysqli->real_escape_string($authorId));
-                        $postContent = $mysqli->real_escape_string($postContent);
-                        //Etape 4 : construction de la requete
-                        $lInstructionSql = "INSERT INTO posts "
-                                . "(id, user_id, content, created, permalink, post_id) "
-                                . "VALUES (NULL, "
-                                . $authorId . ", "
-                                . "'" . $postContent . "', "
-                                . "NOW(), "
-                                . "'', "
-                                . "NULL);"
-                                ;
-                        echo $lInstructionSql;
-                        // Etape 5 : execution
-                        $ok = $mysqli->query($lInstructionSql);
-                        if ( ! $ok)
-                        {
-                            echo "Impossible d'ajouter le message: " . $mysqli->error;
-                        } else
-                        {
-                            echo "Message posté en tant que :" . $listAuteurs[$authorId];
-                        }
-                    }
-                    ?>                     
-                    <form action="usurpedpost.php" method="post">
-                        <input type='hidden' name='???' value='achanger'>
-                        <dl>
-                            <dt><label for='auteur'>Auteur</label></dt>
-                            <dd><select name='auteur'>
-                                    <?php
-                                    foreach ($listAuteurs as $id => $alias)
-                                        echo "<option value='$id'>$alias</option>";
-                                    ?>
-                                </select></dd>
-                            <dt><label for='message'>Message</label></dt>
-                            <dd><textarea name='message'></textarea></dd>
-                        </dl>
-                        <input type='submit'>
-                    </form>               
-                </article>
-            </main>
-        </div>
-    </body>
 </html>
